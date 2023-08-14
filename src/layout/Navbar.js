d82d2befe287510d523564130cf2c4bf90f1389d
dmilosevic3 <dominik.milosevic@fina.hr>
2023-08-14 15:43:12 +0200

    - Kreiranje navigacije - Kreiranje home page-a

---
+++ src/layout/Navbar.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export default function Navbar() {
+    return (
+        <div>
+            <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+                <div className="container-fluid">
+                    <a className="navbar-brand" href="#">Exercise Recorder</a>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+
+                    <button type="button" className="btn btn-outline-light">Add New Exercise</button>
+                </div>
+            </nav>
+        </div>
+    )
+}
