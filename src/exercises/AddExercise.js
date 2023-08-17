99ea86765d44f371b74b677f79fa157d61c2386d
Dominik Milosevic <dmilosevi@tvz.hr>
2023-08-17 19:17:50 +0200

    - djelomična implementacija za spremanje podataka u bazu (POST)

--- src/exercises/AddExercise.js
+++ src/exercises/AddExercise.js
@@ -1,3 +1,4 @@
+import axios from 'axios'
 import React, { useState } from 'react'
 
 export default function AddExercise() {
@@ -16,33 +17,42 @@ export default function AddExercise() {
     setExercise({...exercise,[event.target.name]:event.target.value})
   }
 
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    await axios.post("http://localhost:8080/exercises/add", exercise)
+  }
+
   return (
     <div className="container">
       <div className="row">
         <div className="col-md-6 offset-md-3 border rounded p-4 mt-2 shadow">
           <h2 className="text-center m-4">Informacije o novoj vježbi</h2>
-          <div className="mb-4">
-            <label htmlFor="ImeNoveVjezbe" className="form-label">Ime vježbe:</label>
-            <input type={"text"} className="form-control" placeholder="Unesite ime nove vježbe" name="name" value={name} onChange={(event) => onInputChange(event)}/>
-          </div>
-          <div className="mb-4">
-            <label htmlFor="MisicinaSkupina" className="form-label">Mišićna skupina:</label>
-            <input type={"text"} className="form-control" placeholder="Unesite mišićnu skupinu koju pogađa" name="bodyPart" value={bodyPart} onChange={(event) => onInputChange(event)}/>
-          </div>
-          <div className="mb-4">
-            <label htmlFor="BrojSerija" className="form-label">Broj serija:</label>
-            <input type={"text"} className="form-control" placeholder="Unesite broj serija" name="numberOfSets" value={numberOfSets} onChange={(event) => onInputChange(event)}/>
-          </div>
-          <div className="mb-4">
-            <label htmlFor="BrojPonavljanja" className="form-label">Broj ponavljanja:</label>
-            <input type={"text"} className="form-control" placeholder="Unesite broj ponavljanja" name="numberOfReps" value={numberOfReps} onChange={(event) => onInputChange(event)}/>
-          </div> 
-          <div className="mb-4">
-            <label htmlFor="RadnaKilaža" className="form-label">Radna kilaža:</label>
-            <input type={"text"} className="form-control" placeholder="Unesite radnu kilažu" name="weight" value={weight} onChange={(event) => onInputChange(event)}/>
-          </div>
-          <button type="submit" className="btn btn-dark">Unesi</button>
-          <button type="submit" className="btn btn-dark mx-2">Odustani</button>
+          
+          <form onSubmit={(event) => onSubmit(event)}>
+            <div className="mb-4">
+              <label htmlFor="ImeNoveVjezbe" className="form-label">Ime vježbe:</label>
+              <input type={"text"} className="form-control" placeholder="Unesite ime nove vježbe" name="name" value={name} onChange={(event) => onInputChange(event)}/>
+            </div>
+            <div className="mb-4">
+              <label htmlFor="MisicinaSkupina" className="form-label">Mišićna skupina:</label>
+              <input type={"text"} className="form-control" placeholder="Unesite mišićnu skupinu koju pogađa" name="bodyPart" value={bodyPart} onChange={(event) => onInputChange(event)}/>
+            </div>
+            <div className="mb-4">
+              <label htmlFor="BrojSerija" className="form-label">Broj serija:</label>
+              <input type={"text"} className="form-control" placeholder="Unesite broj serija" name="numberOfSets" value={numberOfSets} onChange={(event) => onInputChange(event)}/>
+            </div>
+            <div className="mb-4">
+              <label htmlFor="BrojPonavljanja" className="form-label">Broj ponavljanja:</label>
+              <input type={"text"} className="form-control" placeholder="Unesite broj ponavljanja" name="numberOfReps" value={numberOfReps} onChange={(event) => onInputChange(event)}/>
+            </div> 
+            <div className="mb-4">
+              <label htmlFor="RadnaKilaža" className="form-label">Radna kilaža:</label>
+              <input type={"text"} className="form-control" placeholder="Unesite radnu kilažu" name="weight" value={weight} onChange={(event) => onInputChange(event)}/>
+            </div>
+            <button type="submit" className="btn btn-dark">Unesi</button>
+            <button type="submit" className="btn btn-dark mx-2">Odustani</button>
+          </form>
+
         </div>
       </div>
     </div>
